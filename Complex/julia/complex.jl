51bae1dfabd15dc5c7f189c9a5f88d87c828a708
tangxiangong <tangxiangong@gmail.com>
2024-12-09 15:57:24 +0800

    Update complex.jl

--- Complex/julia/complex.jl
+++ Complex/julia/complex.jl
@@ -14,8 +14,6 @@ Base.abs2(z::ComplexNum)::Float64 = z.real * z.real + z.imaginary * z.imaginary
 
 Base.abs(z::ComplexNum)::Float64 = sqrt(abs2(z))
 
-
-
 Base.:+(lhs::ComplexNum, rhs::ComplexNum)::ComplexNum = ComplexNum(lhs.real + rhs.real, lhs.imaginary + rhs.imaginary)
 
 function Base.:+(lhs::ComplexNum, rhs::T)::ComplexNum where {T <: Real}
