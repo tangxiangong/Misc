005d87439271520ab5d1611794f71c983caf8f4b
tangxiangong <tangxiangong@gmail.com>
2024-12-13 19:57:56 +0800

    update

--- Complex/rust/src/main.rs
+++ Complex/rust/src/main.rs
@@ -4,7 +4,7 @@ use complex::Complex;
 
 
 fn main() {
-    let z1 = Complex::new(1 as f64, 0 as f64);
+    let z1 = Complex::new(1f64, 0 as f64);
     let z2 = Complex::polar(1.1, 2.2);
     let z3 = z1 + z2;
     println!("{:?}", z1);
