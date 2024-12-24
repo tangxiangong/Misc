2ee334bc8cc55730c531ff3c5d4c475ef0d3eca9
tangxiangong <tangxiangong@gmail.com>
2024-12-24 21:01:48 +0800

    修改逻辑错误

--- FDESolver/fdesolver_jl/src/fdesolver_jl.jl
+++ FDESolver/fdesolver_jl/src/fdesolver_jl.jl
@@ -5,11 +5,11 @@ import SpecialFunctions: gamma
 export FDE, Discretization, solve
 
 struct FDE{F<:Function}
-    fractional_order::Float64
-    time_length::Float64
-    initial_value::Float64
-    spectral_value::Float64
-    source_term::F
+    order::Float64
+    inteval::Float64
+    init::Float64
+    spectrum::Float64
+    source::F
 end
 
 struct Discretization
@@ -19,8 +19,8 @@ end
 
 function solve(discretization::Discretization)
     τ, eq = discretization.τ, discretization.eq
-    α, T, x₀, λ = eq.fractional_order, eq.time_length, eq.initial_value, eq.spectral_value
-    f = eq.source_term
+    α, T, x₀, λ = eq.order, eq.inteval, eq.init, eq.spectrum
+    f = eq.source
     nums = floor(Int, T/τ)
     b = map(0:nums-1) do j 
         ((j+1)^(1-α) - j^(1-α))/gamma(1-α)
@@ -33,7 +33,7 @@ function solve(discretization::Discretization)
     @fastmath @inbounds for n in 2:nums+1
         t[n] = (n-1) * τ 
         temp = mapreduce((x,y)->x*y, +, view(c, 1:n-2), Iterators.reverse(view(x, 2:n-1)), init=0.0)
-        x[n] = (τ^α * f(t[n]) + b[n-1] * x[1] + temp) / a
+        x[n] = (τ^α * f(t[n]) + b[n-1] * x[1] - temp) / a
     end
     t, x
 end
