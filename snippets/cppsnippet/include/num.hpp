47f678f193d73e637bca27000137ca6edcca600f
tangxiangong <tangxiangong@gmail.com>
2024-12-28 19:46:18 +0800

    tyoe trait

---
+++ snippets/cppsnippet/include/num.hpp
@@ -0,0 +1,41 @@
+template <typename T>
+struct is_number {
+    constexpr static bool value = false;
+};
+
+template <typename T>
+constexpr bool is_number_v = is_number<T>::value;
+
+#define DEFINE_IS_NUMBER_FOR_TYPE(T) \
+    template <> \
+    struct is_number<T> { \
+        constexpr static bool value = true; \
+    };
+
+DEFINE_IS_NUMBER_FOR_TYPE(short);
+DEFINE_IS_NUMBER_FOR_TYPE(int);
+DEFINE_IS_NUMBER_FOR_TYPE(long);
+DEFINE_IS_NUMBER_FOR_TYPE(long long);
+DEFINE_IS_NUMBER_FOR_TYPE(unsigned short);
+DEFINE_IS_NUMBER_FOR_TYPE(unsigned int);
+DEFINE_IS_NUMBER_FOR_TYPE(unsigned long);
+DEFINE_IS_NUMBER_FOR_TYPE(unsigned long long);
+DEFINE_IS_NUMBER_FOR_TYPE(float);
+DEFINE_IS_NUMBER_FOR_TYPE(double);
+
+
+template<typename T>
+concept Num = is_number_v<T> && requires (T a, T b) {
+    {a + b};
+    {a - b};
+    {a * b};
+    {a / b};
+};
+
+template<Num T, Num U> requires requires (T a, U b) {
+    a + b;
+}
+
+// auto add(T a, U b) {
+//     return a + b;
+// }
---
+++ snippets/cppsnippet/src/main.cpp
@@ -0,0 +1,8 @@
+#include<print>
+#include<type_traits>
+#include "num.hpp"
+
+int main() {
+    add(1 , 2);
+    std::println("Hello World!");
+}
